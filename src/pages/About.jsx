0ad309a07d5f498914c9127825bf7596eafe8aae
nositelshtanov <groshev523000@gmail.com>
2022-08-25 21:21:03 +0300

    add about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import H1 from "../components/UI/H1/H1";
 
 
 const About = () => {
     return (
-        <div>
-            ABOUT PAGE
-        </div>
+        <>
+            <H1 text={"О сайте"} />
+            <div className={"about_page_block"}>
+                <h2>Сайт сделан в учебных целях!</h2>
+                <p>Покупка товаров на данном сайте не является настоящей</p>
+                <p>Все данные взяты с сайта <a href="https://proskate.ru/">proskate.ru/</a></p>
+                <p>Разработчик: <a href="mailto:groshev523000@gmail.com">groshev523000@gmail.com</a></p>
+            </div>
+        </>
     );
 };
 
